3bb3274e2f251f67d6c2f0ee47ce306de8081303
Topgovina <benjamingovina@outlook.com>
2026-07-27 21:56:52 +0000

    Navigation bar adjustment
    Making the navigation bar look more professional

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
